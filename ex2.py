201b71da0bb95cb7522ad9450d8e6a0d567cf11b
karuiEden <97159954+karuiEden@users.noreply.github.com>
2025-11-13 16:14:07 +0300

    Exercise 2 + Jordan Form

---
+++ ex2.py
@@ -0,0 +1,30 @@
+import sympy as sp
+
+def cell_size(A, lam, k):
+    B = (A - lam * sp.eye(A.shape[0]))
+    return (B ** (k - 1)).rank() + (B ** (k + 1)).rank() - 2 * (B ** k).rank()
+
+def jordan_form(A):
+    vals = A.eigenvals()
+    print(f'Алгебраические кратности собственных чисел {list(vals.keys())} равна {list(vals.values())}')
+    rhos = []
+    for val in vals.items():
+        rho = len((A - val[0] * sp.eye(A.shape[0])).nullspace())
+        print(f'Геометрическая кратность собственного числа {val[1]} равна {rho}\n')
+        rhos.append(rho)
+        for i in range(1, val[1] + 1):
+            quantity = cell_size(A, val[0], i)
+            if quantity > 0:
+                print(f'Количество клеток числа {val[0]} размером {i} равно {quantity}')
+        print()
+    print(f'Исходя из суммарной геометрической кратности, количество Жордановых клеток равно {sum(rhos)}')
+    print('Жордановы клетки:')
+    sp.pprint(A.jordan_cells()[1])
+    print('Жорданова форма:')
+    sp.pprint(A.jordan_form()[1])
+
+if __name__ == '__main__':
+    A = sp.Matrix(
+    [[0, 0, 1, -1, 0, 1], [1, 0, -3, 1, -1, -3], [0, 1, 3, 0, 1, 2], [0, 0, 0, 0, 0, 1], [0, 0, 0, 1, 0, -3],
+         [0, 0, 0, 0, 1, 3]])
+    jordan_form(A)
